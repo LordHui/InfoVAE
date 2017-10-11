4fa3196ad177e8f71c5d8d12eda6d66b0ed60243
Shengjia Zhao <zhaosj12@stanford.edu>
2017-10-10 21:26:35 -0700

    Add cmd

--- elbo_mog.py
+++ elbo_mog.py
@@ -2,8 +2,20 @@ import math
 import numpy as np
 import time
 import tensorflow as tf
+import matplotlib
+matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 import subprocess
+import argparse
+
+parser = argparse.ArgumentParser()
+# python coco_transfer2.py --db_path=../data/coco/coco_seg_transfer40_30_299 --batch_size=64 --gpu='0' --type=mask
+
+parser.add_argument('-m', '--max_reg', type=float, default=1.0, help='Maximum coefficient for KL(q(z|x)||p(z))')
+parser.add_argument('-n', '--nll_bound', type=float, default=-3.0, help='Lower bound on nll')
+parser.add_argument('-g', '--gpu', type=str, default='3', help='GPU to use')
+parser.add_argument('-i', '--nll_iter', type=int, default=25000, help='Number of iterations for log likelihood evaluation')
+args = parser.parse_args()
 
 
 # Define some handy network layers
@@ -71,8 +83,6 @@ def compute_mmd(x, y):
 import os
 os.environ['CUDA_VISIBLE_DEVICES'] = '2'
 reg = 'kl'
-reg_max = 0.5
-nll_lb = -3.0
 
 x_dim = 1
 z_dim = 1
@@ -102,7 +112,7 @@ mmd_reg = compute_mmd(true_samples, train_zsample)
 loss_nll = 0.5 * math.log(2 * math.pi) + tf.log(train_xstddev) + \
     tf.square(train_xmean - train_x) / (2 * tf.square(train_xstddev))
 loss_nll = tf.reduce_mean(tf.reduce_sum(loss_nll, axis=1))
-loss_nll = tf.maximum(loss_nll, nll_lb)
+loss_nll = tf.maximum(loss_nll, args.nll_bound)
 
 if reg == 'kl':
     loss = loss_nll + kl_reg * kl_anneal
@@ -118,6 +128,7 @@ train_summary = tf.summary.merge([
 ])
 true_nll_ph = tf.placeholder(tf.float32)
 true_ll_summary = tf.summary.scalar('true_nll', true_nll_ph)
+
 
 class MoG:
     def __init__(self, centers=None, stddev=0.5):
@@ -151,12 +162,12 @@ def compute_true_nll():
         batch_x, _ = mog.sample(batch_size)
         batch_x = batch_x.reshape(-1, x_dim)
         nll_list = []
-        num_iter = 25000
+        num_iter = args.nll_iter
         for k in range(num_iter):
             random_z = np.random.normal(size=[batch_size, z_dim])
             nll = sess.run(sample_nll, feed_dict={train_x: batch_x, gen_z: random_z})
             nll_list.append(nll)
-            if k % 5000 == 0:
+            if k % 50 == 0:
                 print("iter %d, current value %.4f, time used %.2f" % (k, compute_log_sum(np.stack(nll_list)), time.time() - start_time))
         nll = compute_log_sum(np.stack(nll_list))
         avg_nll.append(nll)
@@ -171,7 +182,9 @@ def make_model_path(name):
     os.makedirs(log_path)
     return log_path
 
-log_path = make_model_path('kl%.2f-%.2f' % (reg_max, nll_lb))
+log_path = make_model_path('kl%.2f-%.2f' % (args.max_reg, args.nll_bound))
+plot_path = os.path.join(log_path, 'plot')
+os.makedirs(plot_path)
 logger = open(os.path.join(log_path, 'log.txt'), 'w')
 writer = tf.summary.FileWriter(log_path, sess.graph)
 writer.flush()
@@ -183,10 +196,10 @@ c_list = np.array(['r', 'g', 'b', 'm'])
 for i in range(1, 10000000):
     batch_x, _ = mog.sample(batch_size)
     batch_x = batch_x.reshape(-1, x_dim)
-    if i < 10000:
+    if i < 2000:
         kl_ratio = 0.01
     else:
-        kl_ratio = reg_max
+        kl_ratio = args.max_reg
     _, total_loss, nll, kl, mmd = sess.run([trainer, loss, loss_nll, kl_reg, mmd_reg],
                                            feed_dict={train_x: batch_x, kl_anneal: kl_ratio})
     if i % 100 == 0:
@@ -195,28 +208,28 @@ for i in range(1, 10000000):
         writer.add_summary(summary, i)
         print("Iteration %d: Loss %f, Negative log likelihood is %f, mmd loss is %f, kl loss is %f" % (i, total_loss, nll, mmd, kl))
     if i % 1000 == 0:
-        # batch_x, mode_index = mog.sample(1000)
-        # batch_x = batch_x.reshape(-1, x_dim)
-        # z_samples = sess.run(train_zsample, feed_dict={train_x: batch_x})
-        # samples = sess.run(gen_xsample, feed_dict={gen_z: np.random.normal(size=(1000, z_dim))})
-        # plt.subplot(1, 3, 1)
-        # if x_dim == 1:
-        #     plt.hist(batch_x, bins=100, color='r', hold=False)
-        #     plt.hist(samples, bins=100, color='b')
-        #     plt.yscale('log')
-        #     plt.xlim([-3, 3])
-        # else:
-        #     plt.scatter(samples[:, 0], samples[:, 1], hold=False)
-        # plt.title('%s: model samples p(x)' % reg)
-        # plt.subplot(1, 3, 2)
-        # if z_dim == 1:
-        #     for modes in range(np.max(mode_index) + 1):
-        #         indices = np.argwhere(np.equal(mode_index, modes))
-        #         plt.hist(z_samples[indices[:, 0], 0], bins=100, color=c_list[modes], hold=not modes == 0)
-        # else:
-        #     plt.scatter(z_samples[:, 0], z_samples[:, 1], c=c_list[np.array(mode_index)], hold=False)
-        # plt.title('%s: latent code q(z)' % reg)
-        # plt.show()
+        batch_x, mode_index = mog.sample(1000)
+        batch_x = batch_x.reshape(-1, x_dim)
+        z_samples = sess.run(train_zsample, feed_dict={train_x: batch_x})
+        samples = sess.run(gen_xsample, feed_dict={gen_z: np.random.normal(size=(1000, z_dim))})
+        if x_dim == 1:
+            plt.hist(batch_x, bins=100, color='r', hold=False)
+            plt.hist(samples, bins=100, color='b')
+            plt.yscale('log')
+            plt.xlim([-3, 3])
+        else:
+            plt.scatter(samples[:, 0], samples[:, 1], hold=False)
+        plt.title('%s: model samples p(x)' % reg)
+        plt.savefig(os.path.join(plot_path, 'model_sample%d.png' % i))
+
+        if z_dim == 1:
+            for modes in range(np.max(mode_index) + 1):
+                indices = np.argwhere(np.equal(mode_index, modes))
+                plt.hist(z_samples[indices[:, 0], 0], bins=100, color=c_list[modes], hold=not modes == 0)
+        else:
+            plt.scatter(z_samples[:, 0], z_samples[:, 1], c=c_list[np.array(mode_index)], hold=False)
+        plt.title('%s: latent code q(z)' % reg)
+        plt.savefig(os.path.join(plot_path, 'latent_code%d.png', i))
 
         true_nll = compute_true_nll()
         logger.write('%d %f %f %f %f\n' % (i, nll, kl, mmd, true_nll))
