71b1acf0bb7fbcf39fd8c4b3c21b94f4cfdd74c6
ShengjiaZhao <sjzhao@stanford.edu>
2018-01-27 19:20:34 -0800

    Deeper network

--- sparse_vae.py
+++ sparse_vae.py
@@ -40,6 +40,7 @@ def encoder(x, z_dim):
         conv2 = conv2d_lrelu(conv1, 128, 4, 2)   # None x 7 x 7 x 128
         conv2 = tf.reshape(conv2, [-1, np.prod(conv2.get_shape().as_list()[1:])]) # None x (7x7x128)
         fc1 = fc_lrelu(conv2, 1024)
+        fc1 = fc_lrelu(fc1, 1024)
         mean = tf.contrib.layers.fully_connected(fc1, z_dim, activation_fn=tf.identity)
         stddev = tf.contrib.layers.fully_connected(fc1, z_dim, activation_fn=tf.sigmoid)
         stddev = tf.maximum(stddev, 0.01)
